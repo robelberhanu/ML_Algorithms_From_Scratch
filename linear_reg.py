ac5c5c5354d35bfe2ca9b4d4d8192589204a0123
Robel Berhanu <inber@srk.co.za>
2023-05-26 13:07:47 +0200

    modified linear_reg.py

---
+++ linear_reg.py
@@ -0,0 +1,14 @@
+
+class LinearRegression:
+    def __init__(self, lr=0.001, n_iters = 1000):
+        self.lr = lr # learning rate
+        self.n_iters = n_iters # number of iterationn for gradient decent
+        self.weights = None
+        self.bias = None
+
+    def fit (self, X, Y):
+        '''
+        This method deals with the learning process - using gradient decent.
+        It takes in the 
+        '''
+        pass
