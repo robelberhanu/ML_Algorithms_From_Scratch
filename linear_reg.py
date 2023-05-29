a153d20728ff32c54779bb001ec92a937f6b2d43
Robel Berhanu <inber@srk.co.za>
2023-05-29 15:14:02 +0200

    updated linear reg model

--- linear_reg.py
+++ linear_reg.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class LinearRegression:
     def __init__(self, lr=0.001, n_iters = 1000):
@@ -9,6 +11,25 @@ class LinearRegression:
     def fit (self, X, Y):
         '''
         This method deals with the learning process - using gradient decent.
-        It takes in the 
+        
         '''
-        pass
+
+        # initialize parameters
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0;
+
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X, self.weights) + bias 
+
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+
+            
+
+
+          
+
+
+
+        
