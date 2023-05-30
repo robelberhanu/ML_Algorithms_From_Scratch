e12c2ddd291147d3ed6c959eebf7a5ea61722254
Robel Berhanu <robelberhanu89@gmail.com>
2023-05-30 15:28:17 +0200

    finalised linear regression model class.

--- linear_reg.py
+++ linear_reg.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, lr=0.001, n_iters = 1000):
+    def __init__(self, lr=0.01, n_iters = 1000):
         self.lr = lr # learning rate
         self.n_iters = n_iters # number of iterationn for gradient decent
         self.weights = None
         self.bias = None
 
-    def fit (self, X, Y):
+    def fit (self, X, y):
         '''
         This method deals with the learning process - using gradient decent.
         
@@ -20,13 +20,16 @@ class LinearRegression:
         self.bias = 0;
 
         for _ in range(self.n_iters):
-            y_predicted = np.dot(X, self.weights) + bias 
+            y_predicted = np.dot(X, self.weights) + self.bias 
 
             dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1/n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
     
     def predict(self, X):
-        y_predicted = np.dot(X, self.weights) + bias 
+        y_predicted = np.dot(X, self.weights) + self.bias 
         return y_predicted
 
             
