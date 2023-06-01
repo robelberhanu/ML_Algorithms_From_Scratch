4b57f0ce096a03d6b9241777e0e99a9bc3cb7c46
Robel Berhanu <robelberhanu89@gmail.com>
2023-06-01 15:14:43 +0200

    added sigmoid helper function in logistic_reg.py

--- logistic regression/logistic_reg.py
+++ logistic regression/logistic_reg.py
@@ -10,6 +10,20 @@ class LogisticRegression:
 
     # this function is designed to train the model using gradient decent. X and Y represent np arrays of training samples and label samples respectively.
     def fit(self, X, Y):
+        n_samples, n_fearures = X.shape
+        self.weights = np.zeros(n_fearures)
+        self.bias = 0
+
+        # gradient decent
+        for _ in range(self.n_iters):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self._sigmoid(linear_model)
+        
+
+    def predict(self, X):
         pass
 
-    def predict(self, X)
+    # Helper function - sigmoid
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
