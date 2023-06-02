3729a5b8bfe98750835af822d44c8015588143b9
Robel Berhanu <robelberhanu89@gmail.com>
2023-06-02 13:46:51 +0200

    modified test data files

---
+++ logistic regression/logistic_regression_test_data.py
@@ -0,0 +1,8 @@
+# This class contains test data samples for training.
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+
