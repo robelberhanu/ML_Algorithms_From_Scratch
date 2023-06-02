5f3b6891f9044f5430bad21c560489380e5f4037
Robel Berhanu <robelberhanu89@gmail.com>
2023-06-02 13:35:15 +0200

    updated gradient decent method in logistic_reg.py to update weights and biases using derivatives of the cost function.

--- logistic regression/logistic_reg.py
+++ logistic regression/logistic_reg.py
@@ -9,15 +9,25 @@ class LogisticRegression:
         self.bias = None
 
     # this function is designed to train the model using gradient decent. X and Y represent np arrays of training samples and label samples respectively.
-    def fit(self, X, Y):
+    def fit(self, X, y):
         n_samples, n_fearures = X.shape
         self.weights = np.zeros(n_fearures)
         self.bias = 0
 
         # gradient decent
         for _ in range(self.n_iters):
+
+            # Define the model
             linear_model = np.dot(X, self.weights) + self.bias
             y_predicted = self._sigmoid(linear_model)
+
+            # update the weight parameters
+            dw = (1/ n_samples) * np.dot(X.T, (y_predicted - y)) # derivative with respect to weights
+            db = (1 / n_samples) * np.sum(y_predicted - y) # derivative with respect to biases
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
         
 
     def predict(self, X):
