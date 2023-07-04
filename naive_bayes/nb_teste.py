66d278955c6241aea97b0b4e86b0874ebccbfe1a
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-04 15:49:04 +0200

    initialised mean, var and priors

--- naive_bayes/nb_teste.py
+++ naive_bayes/nb_teste.py
@@ -4,6 +4,14 @@ class NaiveBayes:
 
     def fit(self, X, y):
         n_samples, n_features = X.shape
+        self.__classes = np.unique(y)
+        n_classes = len(self.__classes)
+
+        #initi mean, var, priors
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.__priors = np.zeros(n_classes, dtype=np.float64)
+
 
     def predict(self, X):
         pass
