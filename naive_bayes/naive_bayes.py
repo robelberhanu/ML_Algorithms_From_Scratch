056c551e10bd53427f2f222e4b6336ce09204489
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-07 12:28:41 +0200

    refactored code to correct mistakes

--- naive_bayes/naive_bayes.py
+++ naive_bayes/naive_bayes.py
@@ -1,18 +1,47 @@
 import numpy as np
+import numpy as np
 
 class NaiveBayes:
 
-
-    def fit(Self, X, y):
+    def fit(self, X, y):
         n_samples, n_features = X.shape
-        self._classes = np.unique(y)
-        n_classes = len(Self._classes)
+        self.__classes = np.unique(y)
+        n_classes = len(self.__classes)
 
-        # initialise mean var, priors
+        #initi mean, var, priors
         self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
-        self._var = np.zeros((n_classes,n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self.__priors = np.zeros(n_classes, dtype=np.float64)
 
-    
+        for c in self.__classes:
+            X_c = X[c==y]
+            self._mean[c,:] = X_c.mean(axis=0)
+            self._var[c,:] = X_c.var(axis=0)
+            self.__priors[c] = X_c.shape[0] / float(n_samples)
+
+
     def predict(self, X):
-        pass
- 
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
+    
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, c in enumerate(self.__classes):
+            prior = np.log(self.__priors[idx])
+            class_conditional =  np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
+        
+        return self.__classes[np.argmax(posteriors)]
+        
+
+    #helper function to calculate probability density function.
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(- (x-mean)**2/(2*var))
+        denominator = np.sqrt(2* np.pi * var)
+        return numerator/denominator
+
+
