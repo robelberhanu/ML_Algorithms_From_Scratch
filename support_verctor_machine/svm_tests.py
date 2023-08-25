a9942cc5167d184d507c70d01074a29cbb086fee
Robel Berhanu <robelberhanu89@gmail.com>
2023-08-25 13:14:59 +0200

    updated fit method - added gradient decent method.

--- support_verctor_machine/svm_tests.py
+++ support_verctor_machine/svm_tests.py
@@ -18,6 +18,17 @@ class SVM:
         self.w = np.zeros(n_features)
         self.b = 0
 
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.lr * y_[idx]
+
+
+
 
     def predict(self, X):
         linear_output = np.dot(X, self.w) - self.b
