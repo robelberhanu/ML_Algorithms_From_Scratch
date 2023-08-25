1844336c36b95109c45e99fcdd38ddd2bf8e422f
Robel Berhanu <robelberhanu89@gmail.com>
2023-08-25 12:58:28 +0200

    initialised SVM class and created fir.

--- support_verctor_machine/svm_tests.py
+++ support_verctor_machine/svm_tests.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 class SVM:
+
+
     def __init__(self, learning_rate = 0.001, lambda_param = 0.01, n_iters = 1000):
         self.lr = learning_rate
         self.lambda_param = lambda_param
         self.n_iters = n_iters
         self.w = None
         self.b = None
+
+    def fit(self, X, y):
+        y_ = np.where(y <= 0, 1)
+        n_samples, n_features = X.shape
+
+
+        self.w = np.zeros(n_features)
+        self.b = 0
+
