a689a53eaf96c7e2ed09d905eb493b20515c2f4e
Robel Berhanu <robelberhanu89@gmail.com>
2023-08-27 22:47:58 +0200

    implemented notch filter class.

--- filter_outliers.py
+++ filter_outliers.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import iirnotch, lfilter
 
 # File name
 filename = "frequency_graph.txt"
@@ -21,14 +23,25 @@ with open(filename, 'r') as file:
         freqs.append(float(freq))
         amplitudes.append(float(amp))
 
+# Define the sample rate and the frequency to be notched out
+fs = 20.0  # Sample rate. This should be adjusted based on your data.
+f0 = 0.5  # Frequency to be removed. Adjust based on your specific requirements.
+Q = 30.0  # Quality factor. Higher values mean narrower notch.
+
+# Design the notch filter
+b, a = iirnotch(f0, Q, fs)
+
+# Apply the filter
+filtered_amplitudes = lfilter(b, a, amplitudes)
+
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.plot(freqs, amplitudes, '-o', markersize=4)
+plt.plot(freqs, filtered_amplitudes, '-o', markersize=4)
 
 # Setting label sizes and boldness
 plt.xlabel("Frequency (Hz)", fontsize=14, fontweight='bold')
 plt.ylabel("Amplitude", fontsize=14, fontweight='bold')
-plt.title("Frequency vs Amplitude", fontsize=16, fontweight='bold')
+plt.title("Frequency vs Amplitude (Filtered)", fontsize=16, fontweight='bold')
 
 # Increasing tick size for readability
 plt.xticks(fontsize=12, fontweight='bold')
