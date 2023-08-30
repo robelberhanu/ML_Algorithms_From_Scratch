9c2cc7b337fba25671831e2f87f9510c64c9d11f
Robel Berhanu <robelberhanu89@gmail.com>
2023-08-30 11:50:52 +0200

    specifies dip value for plots and added code to save figures as svg format.

---
+++ plot_freq.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+# File name
+filename = ""
+
+# Lists to store values
+freqs = []
+amplitudes = []
+
+# Read data from file
+with open(filename, 'r') as file:
+    # Skip the header
+    next(file)
+    
+    # Read each line
+    for line in file:
+        # Split values at whitespace
+        values = line.split()
+        if len(values) == 2:
+            freq, amp = values
+            
+            # Convert strings to float and append to lists
+            freqs.append(float(freq))
+            amplitudes.append(float(amp))
+
+# Plotting
+plt.figure(figsize=(10, 6), dpi=300)  # Specify the DPI here
+plt.plot(freqs, amplitudes, '-o', markersize=4, color='navy')  # using navy blue for the line and markers
+
+# Setting label sizes and boldness
+plt.xlabel("Frequency (Hz)", fontsize=14, fontweight='bold')
+plt.ylabel("Amplitude", fontsize=14, fontweight='bold')
+plt.title("Frequency vs Amplitude", fontsize=16, fontweight='bold')
+
+# Increasing tick size for readability
+plt.xticks(fontsize=12, fontweight='bold')
+plt.yticks(fontsize=12, fontweight='bold')
+
+plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
+plt.tight_layout()
+
+# Save the figure in SVG format
+plt.savefig("output_plot.svg", format='svg')
+
+plt.show()
