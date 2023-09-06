c1ce0daa21f65770df312e2b454d9cd18fe3ec19
Robel Berhanu <robelberhanu89@gmail.com>
2023-09-06 12:23:03 +0200

    updated the frequency-amplitude plotted so that it now considers two sources.

--- plot_freq.py
+++ plot_freq.py
@@ -1,47 +1,68 @@
 import matplotlib.pyplot as plt
+
+# Set the global font to be Times New Roman
 plt.rcParams["font.family"] = "Times New Roman"
 
+# File names
+filename1 = "your_file1.txt"
+filename2 = "your_file2.txt"
 
-# File name
-filename = "stack_frequency_data/reprocessed_sill_frequency.txt"
+# Function to extract data from file
+def extract_data(filename):
+    freqs = []
+    amplitudes = []
 
-# Lists to store values
-freqs = []
-amplitudes = []
+    with open(filename, 'r') as file:
+        # Skip the header
+        next(file)
 
-# Read data from file
-with open(filename, 'r') as file:
-    # Skip the header
-    next(file)
-    
-    # Read each line
-    for line in file:
-        # Split values at whitespace
-        values = line.split()
-        if len(values) == 2:
-            freq, amp = values
-            
-            # Convert strings to float and append to lists
-            freqs.append(float(freq))
-            amplitudes.append(float(amp))
+        # Read each line
+        for line in file:
+            # Split values at whitespace
+            values = line.split()
+            if len(values) == 2:
+                freq, amp = values
+
+                # Convert strings to float
+                freq = float(freq)
+                amp = float(amp)
+
+                # Only append values if amplitude is less than or equal to 0.8
+                if amp <= 0.8:
+                    freqs.append(freq)
+                    amplitudes.append(amp)
+
+    return freqs, amplitudes
+
+# Extract data from the two files
+freqs1, amplitudes1 = extract_data(filename1)
+freqs2, amplitudes2 = extract_data(filename2)
 
 # Plotting
-plt.figure(figsize=(12, 8))  # Specify the DPI here
-plt.plot(freqs, amplitudes, '-', markersize=4, color='navy')  # using navy blue for the line and markers
+plt.figure(figsize=(10, 6), dpi=300)  # Specify the DPI here
 
-# Setting label sizes and boldness
-plt.xlabel("Frequency (Hz)", fontsize=25, fontweight='bold')
-plt.ylabel("Amplitude (m)", fontsize=25, fontweight='bold')
-plt.title("Frequency vs Amplitude (Legacy Stack)", fontsize=26, fontweight='bold')
+# Plot data from the first file
+plt.plot(freqs1, amplitudes1, '-o', markersize=4, color='navy', label='Source 1')
 
-# Increasing tick size for readability
-plt.xticks(fontsize=23, fontweight='bold')
-plt.yticks(fontsize=23, fontweight='bold')
+# Plot data from the second file
+plt.plot(freqs2, amplitudes2, '-o', markersize=4, color='green', label='Source 2')
+
+# Setting label sizes, boldness, and adding units to amplitude
+plt.xlabel("Frequency (Hz)", fontsize=14, fontweight='bold')
+plt.ylabel("Amplitude (m)", fontsize=14, fontweight='bold')
+plt.title("Frequency vs Amplitude", fontsize=16, fontweight='bold')
+
+# Adjusting tick size for readability and setting font size to 14
+plt.xticks(fontsize=14, fontweight='bold')
+plt.yticks(fontsize=14, fontweight='bold')
 
 plt.grid(True, which='both', linestyle='--', linewidth=0.5, color='gray')
 plt.tight_layout()
 
+# Add legend to differentiate between the two datasets
+plt.legend(fontsize=12)
+
 # Save the figure in SVG format
-plt.savefig("reprocessed_sill_frequency_plot.svg", format='svg')
+plt.savefig("output_plot.svg", format='svg')
 
 plt.show()
