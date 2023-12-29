e21157237922d7eaec19d2eb16d4f72881dfe31d
Robel Berhanu <robelberhanu89@gmail.com>
2023-12-29 19:00:37 +0200

    adding comments

--- naive_bayes/nb_teste.py
+++ naive_bayes/nb_teste.py
@@ -1,19 +1,32 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
-import matplotlib.pyplot as plt
+# Importing the numpy library for numerical operations
 
-from naive_bayes import  NaiveBayes
+from sklearn.model_selection import train_test_split
+# Importing train_test_split function to divide the data into training and testing sets
+
+from sklearn import datasets
+# Importing datasets from scikit-learn to generate a synthetic dataset
+
+import matplotlib.pyplot as plt
+# Importing matplotlib for potential data visualization (not used in this script)
+
+from naive_bayes import NaiveBayes
+# Importing the NaiveBayes class you defined earlier
 
 def accuracy(y_true, y_pred):
-    accuracy = np.sum(y_true == y_pred)/len(y_true)
+    # Defining a function to calculate accuracy
+    accuracy = np.sum(y_true == y_pred) / len(y_true)  # Calculates the accuracy as the proportion of correct predictions
     return accuracy
 
+# Generating a synthetic dataset with 1000 samples, 10 features, 2 classes, and a fixed random state
 X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 123)
 
-nb = NaiveBayes()
-nb.fit(X_train, y_train)
-predictions = nb.predict(X_test)
+# Splitting the dataset into training (80%) and testing (20%) sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
+nb = NaiveBayes()  # Creating an instance of the NaiveBayes class
+nb.fit(X_train, y_train)  # Training the NaiveBayes model with the training data
+predictions = nb.predict(X_test)  # Predicting the labels of the test data
+
+# Printing the accuracy of the Naive Bayes classifier on the test data
 print("Naive Bayes classification accuracy", accuracy(y_test, predictions))
